f3806064f9df8d8a3ef2a57c8bd28e79e247be63
37471927 <le-raunack@users.noreply.github.com>
2019-12-23 14:47:02 +0530

    Updates

---
+++ precache-manifest.aac5532b0c9257c5c7621267a1920e6b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0aa5910b4958eded25817b4ac34cfc68",
+    "url": "/Fav-Movies/index.html"
+  },
+  {
+    "revision": "e90d721d10b35831d7d2",
+    "url": "/Fav-Movies/static/css/main.062768e3.chunk.css"
+  },
+  {
+    "revision": "c1c35f368710b3e128eb",
+    "url": "/Fav-Movies/static/js/2.2b1073f1.chunk.js"
+  },
+  {
+    "revision": "e90d721d10b35831d7d2",
+    "url": "/Fav-Movies/static/js/main.1bdb497d.chunk.js"
+  },
+  {
+    "revision": "4ba654ffa824abf17967",
+    "url": "/Fav-Movies/static/js/runtime-main.b9e445d2.js"
+  }
+]);
